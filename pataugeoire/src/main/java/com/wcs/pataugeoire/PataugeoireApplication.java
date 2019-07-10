8060b7f71c098a00aaa012bee53e578b8833fcd1
Lea Goarant <leagoarant@msn.com>
2019-07-10 11:28:10 +0200

    Ajout initializer

---
+++ pataugeoire/src/main/java/com/wcs/pataugeoire/PataugeoireApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.pataugeoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PataugeoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PataugeoireApplication.class, args);
+	}
+
+}
