6f1d6d068d9075dec0ed33a976f3832911a694b0
cecil millerioux <cecil.millerioux@gmail.com>
2019-07-10 16:33:26 +0200

    modif sur homeController

--- src/main/java/com/wcs/Controllers/HomeController.java
+++ src/main/java/com/wcs/Controllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model ) {
         return "index";
     }
 
