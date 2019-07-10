fad37b99e85ada7fe13ff737c2958c5113ad5d0a
Lea Goarant <leagoarant@msn.com>
2019-07-10 15:30:35 +0200

    Ajout entities et boats

---
+++ src/main/java/com/wcs/entities/Boat.java
@@ -0,0 +1,48 @@
+package main.java.com.wcs.entities;
+
+public class Boat{
+    private String pedalo;
+    private String gonflable;
+    private String barque;
+    private String aviron;
+
+    public String getPedalo() {
+        return pedalo;
+    }
+
+    public void setPedalo(String pedalo) {
+        this.pedalo = pedalo;
+    }
+
+    public String getGonflable() {
+        return gonflable;
+    }
+
+    public void setGonflable(String gonflable) {
+        this.gonflable = gonflable;
+    }
+
+    public String getBarque() {
+        return barque;
+    }
+
+    public void setBarque(String barque) {
+        this.barque = barque;
+    }
+
+    public String getAviron() {
+        return aviron;
+    }
+
+    public void setAviron(String aviron) {
+        this.aviron = aviron;
+    }
+
+    public Boat(String pedalo, String gonflable, String barque, String aviron) {
+        this.pedalo = pedalo;
+        this.gonflable = gonflable;
+        this.barque = barque;
+        this.aviron = aviron;
+    }
+    
+}
