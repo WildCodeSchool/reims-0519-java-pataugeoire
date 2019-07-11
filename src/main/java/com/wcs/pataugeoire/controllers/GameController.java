7ec8996e8464c67e90b66c680a49e1995eab499e
Lea Goarant <leagoarant@msn.com>
2019-07-11 15:04:31 +0200

    ajout css et bateaux

--- src/main/java/com/wcs/pataugeoire/controllers/GameController.java
+++ src/main/java/com/wcs/pataugeoire/controllers/GameController.java
@@ -12,19 +12,47 @@ public class GameController{
     @GetMapping("/game")
     public String game(Model model) {
         int[][] boats1 = {
-            {2, 3},
-            {3, 4},
-            {2, 5},
-            {5, 6}
+            {2, 1},
+            {2, 2},
+            {2, 3}
         };
         int[][] boats2 = {
-            {5, 5},
-            {4, 2},
-            {6, 3},
-            {1, 1}
+            {6, 1}
+        };
+        int[][] boats3 = {
+            {2, 6},
+            {3, 6},
+            {4, 6}
+        };
+        int[][] boats4 = {
+            {5, 3},
+            {5, 4}
+        };
+        int[][] boats5 = {
+            {3, 6},
+            {4, 6},
+            {5, 6}
+        };
+        int[][] boats6 = {
+            {6, 1}
+        };
+        int[][] boats7 = {
+            {2, 2},
+            {3, 2}
+        };
+        int[][] boats8 = {
+            {1, 6},
+            {1, 5},
+            {1, 4}
         };
         model.addAttribute("boats1", boats1);
         model.addAttribute("boats2", boats2);
+        model.addAttribute("boats3", boats3);
+        model.addAttribute("boats4", boats4);
+        model.addAttribute("boats5", boats5);
+        model.addAttribute("boats6", boats6);
+        model.addAttribute("boats7", boats7);
+        model.addAttribute("boats8", boats8);
         return "game";
     }
 
