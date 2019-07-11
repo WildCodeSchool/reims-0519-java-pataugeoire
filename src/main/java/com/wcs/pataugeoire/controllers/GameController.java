32f1e929ae09864b573042c62b027123f1a384c8
Lea Goarant <leagoarant@msn.com>
2019-07-11 14:25:15 +0200

    Ajout des bateaux, initialisation de la grille

--- src/main/java/com/wcs/pataugeoire/controllers/GameController.java
+++ src/main/java/com/wcs/pataugeoire/controllers/GameController.java
@@ -11,11 +11,20 @@ public class GameController{
 
     @GetMapping("/game")
     public String game(Model model) {
-        int[][] boats = {
+        int[][] boats1 = {
             {2, 3},
-            {3, 4}
+            {3, 4},
+            {2, 5},
+            {5, 6}
         };
-        model.addAttribute("boats", boats);
+        int[][] boats2 = {
+            {5, 5},
+            {4, 2},
+            {6, 3},
+            {1, 1}
+        };
+        model.addAttribute("boats1", boats1);
+        model.addAttribute("boats2", boats2);
         return "game";
     }
 
